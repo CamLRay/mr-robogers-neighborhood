ed8c48b297b7f1f5052630115144a9500ad85e4f
Sploob <crayiphone@gmail.com>
2022-02-10 19:16:40 -0800

    Pass test 8

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,20 @@ function numberGenerator(num){
   return numArray
 }
 
-function numReplace(numArray){
-let beep = numArray.map(function(num){
-  if (num.toString().includes(1) && !num.toString().includes(2) && !num.toString().includes(3) ) {
-    return "Beep!";
-  } else if (num.toString().includes(2) && !num.toString().includes(3)) {
-    return "Boop!";
-  } else if (num.toString().includes(3)) {
-    return "Won't you be my neighbor?"
-  }
-  return num;
-});
-return beep
+function numReplace(numArray,name){
+  let beep = numArray.map(function(num){
+    if (num.toString().includes(1) && !num.toString().includes(2) && !num.toString().includes(3) ) {
+      return "Beep!";
+    } else if (num.toString().includes(2) && !num.toString().includes(3)) {
+      return "Boop!";
+    } else if (num.toString().includes(3) && name === "") {
+      return "Won't you be my neighbor?"
+    } else if (num.toString().includes(3) && name !== "") {
+      return "Won't you be my neighbor, " + name + "?"
+    }
+    return num;
+  });
+  return beep
 }
 
 
@@ -29,7 +31,8 @@ $(document).ready(function(){
   $("form#formOne").submit(function(e){
     e.preventDefault();
     const num = parseInt($("#userNum").val());
-    let result = numReplace(numberGenerator(num)).join(" ");
+    const name = $("#userName").val().trim();
+    let result = numReplace(numberGenerator(num),name).join(" ");
     if (!isNaN(num)) {
       $("#output").text("Mr. Roboger sings: " + result + ".");
     } else {
